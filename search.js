7b979c157aabc9f020b730b3936c721e87dda233
anniesnoopymd <anniesnoopymd@gmail.com>
2017-10-21 16:39:34 +0800

    update code

--- search.js
+++ search.js
@@ -1,60 +1,83 @@
-var post_api = "https://awiclass.monoame.com/api/command.php?type=get&name=post";
+"use strict";
 
-Vue.component("postbox",{
-   template: "#post",
-   props: ["post"],
-   computed: {
-      coverurl (){
-         console.log(this.post.cover);
-         if(this.post.cover.indexOf("http")!=-1){
-            return this.post.cover;
-         }else{
-            return "http://zashare.org" + this.post.cover;
-         }
-      },
-      covercss (){
-         return {"background-image":'url('+this.coverurl+')'};
-      }
-   }
+var post_api_url = "https://awiclass.monoame.com/api/command.php?type=get&name=post";
+
+Vue.component('postbox', {
+  template: "#post",
+  props: ['post', 'catas'],
+  data: function data() {
+    return {};
+  },
+
+  computed: {
+    coverurl: function coverurl() {
+      if (this.post.cover[0] == "/") return "http://zashare.org/" + this.post.cover;else return this.post.cover;
+    },
+    covercss: function covercss() {
+      return { 'background-image': 'url(' + this.coverurl + ')' };
+    },
+    cata_name: function cata_name() {
+      var _this = this;
+
+      return this.catas.filter(function (cata) {
+        return cata.tag == _this.post.tag;
+      })[0].name;
+    }
+  },
+  methods: {
+    openlink: function openlink() {
+      window.open("http://zashare.org/post/n/" + this.post.title);
+    }
+  }
 });
 var vm = new Vue({
-   el: "#app",
-   data: {
-      posts: [],
-      filter: ""
-   },mounted (){
-      var vobj = this;
-      $.get(post_api).then(function(res){
-         console.log(res);
-         vobj.posts = JSON.parse(res);
-      });
-   },computed:{
-      filtered_post: function(){
-         var vobj = this;
-         return this.posts.filter((post)=>{
-            var tag=["title","description","name_short"];
-            var flag = false;
-            tag.forEach(function(now_tag){
-               console.log(post[now_tag]);
-               if (post[now_tag].indexOf(vobj.filter)!= -1){
-                  flag = true;
-               }
-            });
-            return flag;
-         }).map(function(post){
-            var temp_post = JSON.parse(JSON.stringify(post));
-            temp_post.description = temp_post.description.substr(0,60);
+  el: "#app",
+  data: {
+    posts: [],
+    catas: [],
+    filter: ""
+  },
+  mounted: function mounted() {
+    var vobj = this;
+    $.get(post_api_url).then(function (res) {
+      vobj.posts = JSON.parse(res);
+    });
+    $.get("https://awiclass.monoame.com/api/command.php?type=get&name=cata").then(function (res) {
+      vobj.catas = JSON.parse(res);
+    });
+  },
 
-            if(vobj.filter==""){
-               return temp_post;
-            }
-            var tag=["title","description","name_short"];
-            tag.forEach(function(now_tag){
-               temp_post[now_tag] = temp_post[now_tag].replace(vobj.filter,"<span class=highlight>"+vobj.filter+"</span>");
-            });
-            temp_post.title = temp_post.title.replace(vobj.filter,"<span class=highlight>"+vobj.filter+"</span>");
-            return temp_post;
-         });
-      }
-   }
+  computed: {
+    filtered_post: function filtered_post() {
+      var _this2 = this;
+
+      return this.posts.filter(function (p) {
+        var field = ["title", "description", "name_short"];
+        var contain_flag = false;
+        field.forEach(function (f) {
+          if (p[f].toLowerCase().indexOf(_this2.filter.toLowerCase()) != -1) contain_flag = true;
+        });
+        return contain_flag;
+      }).map(function (p) {
+        p.description = p.description.substr(0, 90) + "...";
+        return p;
+      }).map(function (p) {
+        if (_this2.filter == "") return p;
+        var cache = JSON.parse(JSON.stringify(p));
+        var field = ["title", "description", "name_short"];
+        field.forEach(function (f) {
+          var regex = new RegExp(_this2.filter, "i");
+          var match = cache[f].match(regex);
+          console.log(match);
+          if (match) cache[f] = cache[f].replace(regex, "<span class='highlight'>" + match[0] + "</span>");
+        });
+
+        console.log(cache.tag);
+        return cache;
+      }).sort(function (a, b) {
+        return a.tag.charCodeAt(0) - b.tag.charCodeAt(0);
+      });
+    }
+  }
+
 });
